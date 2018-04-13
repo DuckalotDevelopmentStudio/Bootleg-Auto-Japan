9455da3720682cc4a5e2cedb1d081a00d5ccca74
LERPish <lucaseruipereirademelo@gmail.com>
2018-04-13 20:10:53 +0100

    AI
    it has AI

---
+++ Assets/Scripts/AI/CarManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarManager : MonoBehaviour {
+
+    public GameObject[] cars;
+    public Transform[] targets;
+    private GameObject pCar;
+
+    private bool DL;
+    private bool DR;
+    private bool UL;
+    private bool UR;
+    private int pFollowing;
+
+    // Use this for initialization
+    void Start () {
+        cars = GameObject.FindGameObjectsWithTag("AI");
+        pCar = GameObject.FindGameObjectWithTag("Player");
+        pFollowing = 0;
+        DL = false;
+        DR = false;
+        UL = false;
+        UR = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < cars.Length; i++)
+        {
+            if(cars[i].GetComponent<CarBehaviour>().isFollowing == false)
+            {
+                Debug.Log("number : " + i);
+                GiveObjective(i);
+            }
+        }
+	}
+
+    void GiveObjective(int i)
+    {
+        if(pFollowing <= 0)
+        {
+            cars[i].GetComponent<CarBehaviour>().GetNewTarget(pCar.transform);
+            pFollowing++;
+            cars[i].GetComponent<CarBehaviour>().pFollowing = true;
+        }
+        else if(DL && DR && UL && UR)
+        {
+            cars[i].GetComponent<CarBehaviour>().GetNewTarget(pCar.transform);
+            pFollowing++;
+            cars[i].GetComponent<CarBehaviour>().pFollowing = true;
+        }
+        else if(DL == false)
+        {
+            cars[i].GetComponent<CarBehaviour>().GetNewTarget(targets[0]);
+            DL = true;
+            cars[i].GetComponent<CarBehaviour>().DL = true;
+        }
+        else if (DR == false)
+        {
+            cars[i].GetComponent<CarBehaviour>().GetNewTarget(targets[1]);
+            DR = true;
+            cars[i].GetComponent<CarBehaviour>().DR = true;
+        }
+        else if (UL == false)
+        {
+            cars[i].GetComponent<CarBehaviour>().GetNewTarget(targets[2]);
+            UL = true;
+            cars[i].GetComponent<CarBehaviour>().UL = true;
+        }
+        else if (UR == false)
+        {
+            cars[i].GetComponent<CarBehaviour>().GetNewTarget(targets[3]);
+            DL = true;
+            cars[i].GetComponent<CarBehaviour>().UR = true;
+        }
+    }
+}
